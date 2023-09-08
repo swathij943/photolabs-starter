f0c968ec40d87413e551c0d7e2a2c449c44bd0e5
swathij943 <swathij943@gmail.com>
2023-09-08 10:10:38 -0400

    Adding Readme.md

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -25,9 +25,13 @@ const TopNavigation = (props) => {
     };
   }, []);
 
+  const handleLogoClick = () => {
+    window.location.reload();
+  };
+
   return (
     <div className={`top-nav-bar fixed top-0 left-0 right-0 z-40 ${isAtTop ? 'bg-white' : 'bg-opacity-75 bg-white'}`}>
-      <span className="top-nav-bar__logo mt-2">PhotoLabs</span>
+      <span className="top-nav-bar__logo mt-2 cursor-pointer" onClick={handleLogoClick}>PhotoLabs</span>
       <div className="flex flex-row items-center mr-6">
 
       <TopicList topics={topics} getPhotosByTopic={getPhotosByTopic} selectedTopic={selectedTopic} />
