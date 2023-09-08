9590c4d95a26c8cd1da5902a25a1c963e7053c3a
swathij943 <swathij943@gmail.com>
2023-09-07 20:28:27 -0400

    Implementing - fetching data from a server/API data and setting up database-postgres

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useApplicationData from './hooks/useApplicationData';
-import topics from './mocks/topics';
-import photos from './mocks/photos';
+// import topics from './mocks/topics';
+// import photos from './mocks/photos';
 import HomeRoute from './components/HomeRoute';
 import PhotoDetailsModal from './components/PhotoDetailsModal';
 import './App.scss';
 
 const App = () => {
-  
+  const [photos, setPhotos] = useState([]);
+  const [topics, setTopics] = useState([]);
+
   const { state, addRemoveLike, openModal, closeModal } = useApplicationData();
   const { likes, selectedImg, isModalOpen } = state;
 
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,10 +1,6 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 const useApplicationData = () => {
-
-  const [likes, setLikes] = useState(["1", "7", "3"]);
-  const [selectedImg, setSelectedImage] = useState(getInitialSelectedImgState);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   function getInitialSelectedImgState() {
     return {
@@ -26,31 +22,77 @@ const useApplicationData = () => {
       similar_photos: []
     };
   }
+  
+  const initialState = {
+    likes: ["1", "7", "3"],
+    selectedImg: getInitialSelectedImgState(),
+    isModalOpen: false
+  };
+  
+  const ACTIONS = {
+    FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+    SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+    SELECT_PHOTO: 'SELECT_PHOTO',
+    DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+    CLOSE_PHOTO_DETAILS: 'CLOSE_PHOTO_DETAILS'
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case ACTIONS.FAV_PHOTO_ADDED:
+        return {
+          ...state,
+          likes: state.likes.includes(action.payload)
+            ? state.likes.filter(likeId => likeId !== action.payload)
+            : [...state.likes, action.payload]
+        };
+
+      case ACTIONS.SET_PHOTO_DATA:
+        return {
+          ...state,
+          selectedImg: {
+            ...state.selectedImg,
+            id: action.payload.id,
+            location: action.payload.location,
+            urls: action.payload.urls,
+            user: action.payload.user,
+            similar_photos: action.payload.similar_photos
+          }
+        };
+
+      case ACTIONS.SELECT_PHOTO:
+        return {
+          ...state,
+          selectedImg: action.payload
+        };
+      case ACTIONS.DISPLAY_PHOTO_DETAILS:
+        return {
+          ...state,
+          isModalOpen: true
+        };
+      case ACTIONS.CLOSE_PHOTO_DETAILS:
+        return {
+          ...state,
+          isModalOpen: false
+        }
+      default:
+        throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   const addRemoveLike = function(id) {
-    setLikes((prevLikes) => {
-      if (prevLikes.includes(id)) {
-        return prevLikes.filter(likeId => likeId !== id);
-      } else {
-        return [...prevLikes, id];
-      }
-    });
+    dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: id });
   };
+
   const openModal = (id, location, urls, user, similar_photos) => {
-    setIsModalOpen(true);
-    setSelectedImage((prev) => {
-      return { ...prev, id, location, urls, user, similar_photos };
-    });
+    dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: { id, location, urls, user, similar_photos } });
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS });
   };
+
   const closeModal = () => {
-    setIsModalOpen(false);
+    dispatch({type: ACTIONS.CLOSE_PHOTO_DETAILS});
   };
-
-  const state = {
-    likes,
-    selectedImg,
-    isModalOpen
-  };
-
   return {
     state,
     addRemoveLike,
@@ -58,6 +100,4 @@ const useApplicationData = () => {
     closeModal
   };
 };
-
 export default useApplicationData;
-
